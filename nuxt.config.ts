69b4e6dde3396578a00dcf380fd0cc45ccda82c0
gayloc <gu18647403665@outlook.com>
2024-08-13 21:36:19 +0800

    关闭ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   ssr: false,
-  generate: {
-    routes: ['/教学楼/0','/教学楼/1','/教学楼/2','/教学楼/3']
-  },
 
   modules: ["vuetify-nuxt-module", "@ant-design-vue/nuxt"]
 })
