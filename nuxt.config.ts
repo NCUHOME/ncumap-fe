63f923bf3a5dbc576729bd1014aa3a4e79f50b3d
gayloc <gu18647403665@outlook.com>
2024-08-13 21:40:31 +0800

    启用ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  ssr: false,
-
   modules: ["vuetify-nuxt-module", "@ant-design-vue/nuxt"]
 })
