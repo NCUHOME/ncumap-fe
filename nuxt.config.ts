488003251f943ae18c5b05d6e0a476bb7a6b5a86
gayloc <gu18647403665@outlook.com>
2024-08-14 16:40:14 +0800

    完成优先级排序

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  devServer: {
-    host: '192.168.31.243'
-  },
-
   modules: ["vuetify-nuxt-module", "@ant-design-vue/nuxt"]
 })
