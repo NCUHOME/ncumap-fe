e8462a89b0a3af869a60b8b8505fa490a1582c1c
gayloc <gu18647403665@outlook.com>
2024-08-23 18:18:32 +0800

    禁止用户缩放

--- app.vue
+++ app.vue
@@ -18,5 +18,6 @@ useSeoMeta({
   ogDescription: 'mapping campus, mapping life! ',
   twitterTitle: '校园地图',
   twitterDescription: 'mapping campus, mapping life! ',
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
 })
 </script>
