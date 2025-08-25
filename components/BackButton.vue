5ac121f14d66b9445c61fd08ef513ec6033c1e42
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2025-08-25 21:53:56 +0800

    fix: 无法返回上一页

--- components/BackButton.vue
+++ components/BackButton.vue
@@ -1,6 +1,6 @@
 <template>
     <v-btn icon width="45px" height="45px" color="rgba(255,255,255,0)"
-        :style="{ position: `fixed`,zIndex: 9999,top: `${40 + marginTop}px`,left: `19px` }" variant="flat" @click="$router.back()">
+        :style="{ position: `fixed`, zIndex: 9999, top: `${40 + marginTop}px`, left: `19px` }" variant="flat" @click="$router.replace('/')">
         <v-img src="/back.svg" width="auto"></v-img>
     </v-btn>
 </template>         
